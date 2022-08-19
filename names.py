a7cc8fb19d902bc570c9b2387fc242befd79d0da
Lydell <lydellaaron@gmail.com>
2022-08-18 18:26:33 -0700

    Python 699 last day commit.

---
+++ names.py
@@ -0,0 +1,17 @@
+def read_sort(filename):
+    with open(filename) as fp:
+        file_text = fp.read()
+        str_names = file_text.splitlines()
+        str_names.sort()
+        return str_names
+
+def main():
+    sorted_names = read_sort("sorted_names.csv")
+    with open("sorted_names.csv", "w") as sfp:
+        for name in sorted_names:
+            sfp.write(f"{name}\n")
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ program.py
@@ -0,0 +1,15 @@
+def read_sort(filename):
+    with open(filename) as fp:
+        file_text = fp.read()
+        str_names = file_text.splitlines()
+        str_names.sort()
+        return str_names
+
+def main():
+    sorted_names = read_sort("sorted_names.csv")
+    with open("sorted_names.csv", "w") as sfp:
+        for name in sorted_names:
+            sfp.write(name + "\n")
+
+if __name__ == "__main__":
+    main()
